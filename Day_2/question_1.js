70a447d3c7ba33d8678c8359f12554a0b618888b
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-23 18:44:29 +0530

    done day 3 assignment

--- Day_2/question_1.js
+++ Day_2/question_1.js
@@ -1,5 +1,16 @@
-let age = prompt("enter your age:");
-if (age > 21)
-    console.log("can drink");
+let marks = prompt("Enter your marks:");
+
+if (marks > 90)
+    console.log("Marks are ", marks, " and grade is A");
+
+else if (marks <= 90 && marks > 75)
+    console.log("Marks are ", marks, " and grade is B");
+
+else if (marks <= 75 && marks > 50)
+    console.log("Marks are ", marks, " and grade is C");
+
+else if (marks <= 50 && marks > 30)
+    console.log("Marks are ", marks, " and grade is D");
+
 else
-    console.log("cannot drink");
+    console.log("Marks are ", marks, " and grade is F");
